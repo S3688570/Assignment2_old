dcc60f09804c2b4e04cf2f2bd8f4a4d2e68a508c
Charles Galea <s3688570@student.rmit.edu>
2018-04-19 14:07:19 +1000

    Generate JavaDoc file

--- src/org/launchcode/MiniNet.java
+++ src/org/launchcode/MiniNet.java
@@ -3,6 +3,21 @@ package org.launchcode;
 import java.util.Scanner;
 
 public class MiniNet {
+    /**
+     * <h1>MiniNet</h1>
+     * MiniNet is a program simulating a social network. A social network, in the simplest sense,
+     * is a collection of connections linking a set of people. Each person has a profile in the
+     * social network. To do this I created a interactive network called MiniNet containing profile
+     * information (i.e. name, age status, profile image, friends, parents and children.
+     * The program allows the user to add, search, modify and delete profiles.
+     * <p>
+     *     <b>Note:</b> This project was an assignment completed for the course Advanced Programming
+     *     as part of the Master of Data Science course at RMIT (Melbourne, Australia).
+     *     @author: Charles Galea
+     *     @version: 1.1
+     *     @since: 2014-04-19
+     * </p>
+     */
     //Created by Charles Galea (March 2018)
 
     public MiniNet() {
--- src/org/launchcode/YoungChild.java
+++ src/org/launchcode/YoungChild.java
@@ -1,4 +1,10 @@
 package org.launchcode;
 
 public class YoungChild {
+    //Created by Charles Galea (April 2018)
+
+    //Create default YoungChild object
+    public YoungChild() {
+
+    }
 }
